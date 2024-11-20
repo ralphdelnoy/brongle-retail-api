1e89d88ac0cd5601b9b90de6fc4187c24eea30b2
ralphdelnoy <ralph.delnoy@gmail.com>
2024-11-20 21:23:01 +0100

    Update apiExample_php.php
    In this version possible timeout restrictions of the requesting server should be prevented by recalling the file while all articles are read in chunks.

--- apiExample_php.php
+++ apiExample_php.php
@@ -1,7 +1,5 @@
 <?php
 
-//NOTE: In this PHP example, possible timeout restrictions of the requesting server are not included - to include them consider AJAX/CRON to read one chunk per file request 
-
 //curl method
 function curl($url){
 	$ch = curl_init();// create curl resource
@@ -21,38 +19,56 @@ $api_domain='https://demo.brongle.com';//fill in the domain of the end-point to 
 $api_key='YOUR-API-KEY';//your api key
 $api_page=1;//page to start with
 $api_fields='id,saved';//article fields to request - a star '*' will result in requesting all avaialable fields
-$data=curl($api_domain.'/brongle/API_RET_V1/articles/feed.php?key='.$api_key.'&page='.$api_page.'&fields='.$api_fields);
-$jsonArr = json_decode($data, true);//decode json string to associative array
 
-if(is_array($jsonArr)){
-	//BEGIN get meta data
-	$meta=$jsonArr['meta'];//array with meta data of json
-	//tranfer meta data to locals
-	$totalPages=$meta['totalPages'];
-	$page=$meta['page'];//the current page number
-	$totalRows=$meta['totalRows'];//the total number of articles that exsists in all pages
-	$rows=$meta['rows'];//the number of articles that exsists in the current page
-	//END get meta data	
-	$api_fields='id,se_brand,se_serie,price';//limit requesting fields to ID, SE_BRAND, SE_SERIE and PRICE
-	//loop trough all pages - (NOTE: this is the part where its recommended (+/- with more then 1000 articles) to to read the underneath chunks with separated file requests instead of reading chunks within this one file request)
-	for($p=1; $p<$totalPages; $p++){
-		$currentPage=$p;
-		$data=curl($api_domain.'/brongle/API_RET_V1/articles/feed.php?key='.$api_key.'&page='.$currentPage.'&fields='.$api_fields);
-		$jsonArr = json_decode($data, true);
+$act=(isset($_GET['act'])) ? $_GET['act'] : 'ini';
+
+if($act=='ini'){
+	$data=curl($api_domain.'/brongle/API_RET_V1/articles/feed.php?key='.$api_key.'&page='.$api_page.'&fields='.$api_fields);
+	$jsonArr = json_decode($data, true);//decode json string to associative array
+	if(is_array($jsonArr)){
 		//BEGIN get meta data
 		$meta=$jsonArr['meta'];//array with meta data of json
-		$rows=$meta['rows'];//the number of articles that exsists in the current page
-		//END get meta data
-		//BEGIN get article data
-		$articles=$jsonArr['DATA'];//array with articles data
-			for($a=0; $a<$rows; $a++){
-				print 'id='.$articles[$a]['id'].' se_brand='.$articles[$a]['se_brand'].' se_serie='.$articles[$a]['se_serie'].' price='.$articles[$a]['price'].'<br>';	
-			}
-		//END get article data	
+		//tranfer meta data to locals
+		$totalPages=$meta['totalPages'];
+		$page=$meta['page'];//the current page number
+		//END get meta data		
+		$act='loadArts';
+	}
+	else{
+		print "no data available";	
 	}
 }
-else{
-	print "no data available";	
+
+
+if($act=='loadArts'){
+	//catch up required get parameters
+	$totalPages=(isset($_GET['totalPages'])) ? $_GET['totalPages'] : $totalPages;
+	$page=(isset($_GET['page'])) ? $_GET['page'] : $page;
+	$api_fields='id,se_brand,se_serie,price';//limit requesting fields to ID, SE_BRAND, SE_SERIE and PRICE
+	$data=curl($api_domain.'/brongle/API_RET_V1/articles/feed.php?key='.$api_key.'&page='.$page.'&fields='.$api_fields);
+	$jsonArr = json_decode($data, true);
+	//BEGIN get meta data
+	$meta=$jsonArr['meta'];//array with meta data of json
+	$rows=$meta['rows'];//the number of articles that exsists in the current page
+	//END get meta data
+	//BEGIN get article data
+	$articles=$jsonArr['DATA'];//array with articles data
+		for($a=0; $a<$rows; $a++){
+			//output for testing
+			print 'id='.$articles[$a]['id'].' se_brand='.$articles[$a]['se_brand'].' se_serie='.$articles[$a]['se_serie'].' price='.$articles[$a]['price'].'<br>';
+			//here your database insert/update handling
+			//..
+		}
+	//END get article data	
+	if($page<$totalPages){
+		$page++;
+		//relaunche this file to load next page
+		print '<meta http-equiv="refresh" content="0 url=apiExample_php.php?totalPages='.$totalPages.'&page='.$page.'">';
+	}
+	else{
+		print "All pages where succesfully loaded<br>";	
+	}
+	
 }
 
 ?>
